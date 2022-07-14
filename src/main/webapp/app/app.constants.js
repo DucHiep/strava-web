fda1da3a16aac98f4a4fdefac255fb0781a4bf5f
Doan NM <doannm@vnpt-technology.vn>
2022-07-14 09:40:37 +0700

    update

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -6,5 +6,5 @@
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', true)
         .constant('BUILD_TIMESTAMP', "")
-        // .constant('HOST_GW', "http://10.15.13.150:9292")})();
-        .constant('HOST_GW', "http://localhost:9292")})();
+        .constant('HOST_GW', "http://10.15.13.150:9292")})();
+        // .constant('HOST_GW', "http://localhost:9292")})();
